148e2fa0ad488a20ee1be4fa49270a1e747fc845
colenerad <co521593@ucf.edu>
2025-04-02 13:56:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,21 @@
-import React, { useState } from "react";
-import RepetitionExercise from "./components/RepetitionExercise";
-import DurationExercise from "./components/DurationExercise";
-import RunningExercise from "./components/RunningExercise";
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './components/Home';
+import DurationExercise from './components/DurationExercise';
+import RepetitionExercise from './components/RepetitionExercise';
 
-function App() {
-  const [selectedExercise, setSelectedExercise] = useState(null);
+const Stack = createStackNavigator();
 
-  const exercises = [
-    { name: "Push-ups", type: "repetition" },
-    { name: "Jumping Jacks", type: "duration" },
-    { name: "Sit-ups", type: "repetition" },
-    { name: "Plank", type: "duration" },
-    { name: "Running", type: "running" },  // New exercise added!
-  ];
-
+export default function App() {
   return (
-<div style={{ textAlign: "center", padding: "20px" }}>
-<h1>Updated Exercise App</h1>
-
-{!selectedExercise ? (
-<>
-<h2>Select an Exercise:</h2>
-{exercises.map((exercise, index) => (
-<button
-key={index}
-onClick={() => setSelectedExercise(exercise)}
-style={{ margin: "5px", padding: "10px" }}
->
-{exercise.name}
-</button>
-))}
-</>
-) : (
-<>
-<button onClick={() => setSelectedExercise(null)} style={{ marginBottom: "20px" }}>
-Back to Menu
-</button>
-{selectedExercise.type === "repetition" ? (
-<RepetitionExercise name={selectedExercise.name} />
-) : selectedExercise.type === "duration" ? (
-<DurationExercise name={selectedExercise.name} />
-) : (
-<RunningExercise name={selectedExercise.name} />
-)}
-</>
-)}
-</div>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="DurationExercise" component={DurationExercise} />
+        <Stack.Screen name="RepetitionExercise" component={RepetitionExercise} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-export default App;
-
