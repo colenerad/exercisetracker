59b119cd13267a34c9b6bab352d272a90f53c836
colenerad <co521593@ucf.edu>
2025-04-02 13:58:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // App.js
 import React from 'react';
+import RunningExercise from './components/RunningExercise';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './components/Home';
@@ -12,10 +13,11 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="DurationExercise" component={DurationExercise} />
-        <Stack.Screen name="RepetitionExercise" component={RepetitionExercise} />
-      </Stack.Navigator>
+  <Stack.Screen name="Home" component={Home} />
+  <Stack.Screen name="DurationExercise" component={DurationExercise} />
+  <Stack.Screen name="RepetitionExercise" component={RepetitionExercise} />
+  <Stack.Screen name="RunningExercise" component={RunningExercise} /> 
+</Stack.Navigator>
     </NavigationContainer>
   );
 }
